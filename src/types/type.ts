c37adefb3dc2fccea228b405daf17946a44af44e
kkkkkSE <eun2abc@naver.com>
2022-03-04 18:50:47 +0900

    [feat] Home 페이지에서 필요한 interface 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,1 +1,24 @@
-export default "";
+export interface caffeinCamp {
+  id: number;
+  name: string;
+  category: "pop" | "sale";
+  startDate: string;
+  status: string;
+  field: string;
+  skill: string;
+  thumbnail: string;
+}
+
+export interface caffeinCommunity {
+  id: number;
+  tags: string[];
+  title: string;
+  content: string;
+  comments: caffeinComment[];
+}
+
+export interface caffeinComment {
+  profileImg: string;
+  nickname: string;
+  content: string;
+}
