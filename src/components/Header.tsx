8cc48cec514e7b274f4b1f9f11139fdc7deb60d3
kkkkkSE <eun2abc@naver.com>
2022-03-05 23:29:32 +0900

    [feat] Header 마크업 및 스타일 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,42 @@
 // eslint-disable-next-line
 import styled from "styled-components";
+import colors from "../styles/color";
 
 const Header = () => {
   return (
     <Container>
-      <div className="wrap"></div>
+      <div className="headerWrap">
+        <span className="logo">로고</span>
+        <div className="myPage"></div>
+      </div>
     </Container>
   );
 };
 
 const Container = styled.div`
-  .wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: ${colors.White};
+  width: 100%;
+
+  .headerWrap {
+    box-sizing: border-box;
     width: 960px;
     height: 60px;
     margin: 0 auto;
-    background-color: #aff;
+    padding: 0 10px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+  .logo {
+  }
+  .myPage {
+    width: 20px;
+    height: 20px;
+    background-color: black;
+    border-radius: 50%;
   }
 `;
 export default Header;
