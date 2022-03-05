0ea658ffe5e6e36bdd50e3e0f17689c67d7b1643
kkkkkSE <eun2abc@naver.com>
2022-03-05 23:24:47 +0900

    [feat] 디자인 시스템 반영

---
+++ src/styles/color.ts
@@ -0,0 +1,15 @@
+export default {
+  Black: "#000000",
+  White: "#FFFFFF",
+  Gray01: "#212121",
+  Gray02: "#616161",
+  Gray03: "#888888",
+  Gray04: "#CCCCCC",
+  Gray05: "#EEEEEE",
+  Gray06: "#F5F5F5",
+  Primary01: "#7471FF",
+  Primary02: "#CECBEF",
+  Primary03: "#8148FB",
+  Secondary01: "#24DBAF",
+  Secondary02: "#CDF3EA",
+};
