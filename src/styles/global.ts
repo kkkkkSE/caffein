19505e0d6109cb48abfa27c87eda281bcadb28a6
kkkkkSE <eun2abc@naver.com>
2022-03-07 21:05:38 +0900

    [feat] global style 생성

---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  a{
+    text-decoration : none;
+    color: inherit;
+  }
+  body{
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
