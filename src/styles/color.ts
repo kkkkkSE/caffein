25ed1c1e79945927c7d87d8d1a06182339f630f9
kkkkkSE <eun2abc@naver.com>
2022-03-09 20:37:08 +0900

    [feat] 디자인 시스템 컬러 수정

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -9,7 +9,8 @@ export default {
   Gray06: "#F5F5F5",
   Primary01: "#7471FF",
   Primary02: "#CECBEF",
-  Primary03: "#8148FB",
+  Primary03:
+    "linear-gradient(97.88deg, #854BFF -84.42%, #7179FF 15.02%, #6D83FF 36.89%, #698BFF 54.79%, #6499FF 80.65%, #659BF7 108.49%, #669FEA 148.27%, #69A9CD 197.99%, #6CB1B2 239.75%)",
   Secondary01: "#24DBAF",
   Secondary02: "#CDF3EA",
 };
