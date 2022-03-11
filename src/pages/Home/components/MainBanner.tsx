a8c1ca33e232863d4f1e9cf1d8979dddc25654af
kkkkkSE <eun2abc@naver.com>
2022-03-12 01:18:18 +0900

    [design] swiper 적용을 위해 스타일 코드 변경

--- src/pages/Home/components/MainBanner.tsx
+++ src/pages/Home/components/MainBanner.tsx
@@ -8,34 +8,34 @@ const MainBanner = () => {
   return (
     <Container>
       <section className="mainBannerSection">
-        <div className="leftPart">
+        <div className="content">
           <p className="bannerTitle">
             개발은
             <br /> 카페인과 함께
           </p>
-          <div className="arrowPart">
-            <div className="line"></div>
-            <div className="leftArrow arrow">
-              <img
-                src="/assets/image/icons/left-btn.svg"
-                alt="배너 왼쪽으로 넘기기"
-              />
-            </div>
-            <div className="rightArrow arrow">
-              <img
-                src={"/assets/image/icons/right-btn.svg"}
-                alt="배너 오른쪽으로 넘기기"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="rightPart">
+
           <div
             className="bannerImage"
             style={{
               backgroundImage: `url("/assets/image/banner-img.png")`,
             }}
           ></div>
+        </div>
+
+        <div className="arrowPart">
+          <div className="line"></div>
+          <div className="leftArrow arrow">
+            <img
+              src="/assets/image/icons/left-btn.svg"
+              alt="배너 왼쪽으로 넘기기"
+            />
+          </div>
+          <div className="rightArrow arrow">
+            <img
+              src={"/assets/image/icons/right-btn.svg"}
+              alt="배너 오른쪽으로 넘기기"
+            />
+          </div>
         </div>
       </section>
     </Container>
@@ -51,21 +51,34 @@ const Container = styled.div`
     ${maxWidth}
     display: flex;
     justify-content: space-between;
+    position: relative;
   }
 
-  .leftPart {
+  .content {
+    width: 100%;
     display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
+    justify-content: space-between;
+    align-items: flex-end;
   }
+
   .bannerTitle {
     font-weight: bold;
     font-size: 48px;
     line-height: 56px;
-    padding-bottom: 20px;
+    padding-bottom: 50px;
+  }
+
+  .bannerImage {
+    width: 600px;
+    height: 400px;
+    background-size: cover;
+    background-position: 50% 50%;
   }
 
   .arrowPart {
+    position: absolute;
+    left: 10px;
+    bottom: 0;
     display: flex;
     align-items: center;
   }
@@ -76,17 +89,6 @@ const Container = styled.div`
   }
   .arrow {
     margin-left: 25px;
-  }
-
-  .rightPart {
-    overflow: hidden;
-    border-radius: 10px;
-  }
-  .bannerImage {
-    width: 600px;
-    height: 400px;
-    background-size: cover;
-    background-position: 50% 50%;
   }
 `;
 export default MainBanner;
